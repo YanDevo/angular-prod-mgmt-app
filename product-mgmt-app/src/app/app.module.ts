176b9b26864b5244d18f28766a8842b2086be5e2
1YanDevo <wadeyannick@gmail.com>
2022-10-20 17:41:20 -0500

    product list update

--- product-mgmt-app/src/app/app.module.ts
+++ product-mgmt-app/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './home/welcome/welcome.component';
+import { ProductListComponent } from './products/product-list/product-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeComponent,
+    ProductListComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
