529257a04ccab714e696c4db733140d5e5ed23be
1YanDevo <wadeyannick@gmail.com>
2022-10-25 13:21:42 -0500

    page routing

--- product-mgmt-app/src/app/app.module.ts
+++ product-mgmt-app/src/app/app.module.ts
@@ -9,6 +9,8 @@ import { WelcomeComponent } from './home/welcome/welcome.component';
 import { ProductListComponent } from './products/product-list/product-list.component';
 import { DashesToSpacesPipe } from 'src/app/shared/dashes-to-spaces.pipe';
 import { StarRatingComponent } from './shared/star-rating/star-rating.component';
+import { ProductDetailComponent } from './products/product-detail/product-detail.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -17,12 +19,20 @@ import { StarRatingComponent } from './shared/star-rating/star-rating.component'
     ProductListComponent,
     DashesToSpacesPipe,
     StarRatingComponent,
+    ProductDetailComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     RatingModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: 'products', component: ProductListComponent},
+      {path: 'products/:id', component: ProductDetailComponent},
+      {path: 'welcome', component: WelcomeComponent},
+      {path: '', redirectTo: 'welcome', pathMatch: 'full'},
+      {path: '**', redirectTo: 'welcome', pathMatch: 'full'},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
